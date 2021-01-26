6f94866414abedefd10fd2aacb871c6e6ad46749
karth <karth@DESKTOP-FKEBO5T>
2021-01-26 14:19:02 +0530

    new class added

---
+++ Sample/src/test/java/Firefoxlaunch.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Firefoxlaunch {
+	@Test
+	public void launch_browser(){
+		WebDriver Driver=new ChromeDriver();
+		
+	}
+
+}
---
+++ Sample/src/test/java/pgm.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.Test;
+
+public class pgm {
+	@Test
+	public void hello() {
+		System.out.println("hello");
+	}
+
+}
